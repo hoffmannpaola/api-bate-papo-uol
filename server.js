42330fded6b8dc628399e1f54ac9ef99d89eab6e
hoffmannpaola <hoffmannpaola@gmail.com>
2020-11-11 21:37:39 -0300

    Started GET messages

--- server.js
+++ server.js
@@ -7,40 +7,66 @@ const dayjs = require('dayjs')
 
 
 // console.log(dayjs().format('HH:mm:ss') )
-
+//push add ao final de um array
 
 server.use(express.json());
 server.use(cors());
 
 let participants = [];
-let cameIntoRoom = [];
+
+const arrayMessages = [ { 
+    from: 'Maravilha',
+    to: 'Todos',
+    text: 'entra na sala...',
+    type: 'status',
+    time: '21:49:20' },
+    { from: 'Motorola',
+    to: 'Todos',
+    text: 'entra na sala...',
+    type: 'status',
+    time: '21:46:33' }
+];
+// console.log(arrayMessages);
 
 server.post("/participants", (req, res) => {
     
     const { name } = req.body;
     
-
     if (name === " ") {
-        res.sendStatus(400);
+        return res.sendStatus(400);
     } else {
         const { result } = stripHtml(name);
-    
-        participants.push({name: result, lastStatus: Date.now()})
-
-        cameIntoRoom.push(
+        participants.push({name: result, lastStatus: Date.now()}) 
+        arrayMessages.push(
             { from: result, 
             to: 'Todos', 
             text: 'entra na sala...', 
             type: 'status', 
             time: dayjs().format('HH:mm:ss')}
-        );
-        
+        ); 
     }
     res.status(200);
-    console.log(participants);
-    console.log(cameIntoRoom);
+    // console.log(participants);
+    
     
 })
 
+
+server.get("/teste", (req, res) => {
+    res.send([ { 
+        from: 'Maravilha',
+        to: 'Todos',
+        text: 'entra na sala...',
+        type: 'status',
+        time: '21:49:20' },
+        { from: 'Motorola',
+        to: 'Todos',
+        text: 'entra na sala...',
+        type: 'status',
+        time: '21:46:33' }
+    ]);
+    // console.log(arrayMessages);
+})
+
 // Configura o servidor para rodar na porta 3000
-server.listen(5000);
+server.listen(3000);
